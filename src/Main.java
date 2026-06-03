c0e2d29afae9383766d81b493f9e18dd1ec20f83
Basile Morin <basilemorin18@gmail.com>
2026-06-03 09:00:36 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        GestionnaireFraudes gestionnaireFraudes;
 
+        System.out.println("Voulez-vous utiliser les données de test ?");
+        System.out.println("1. Oui");
+        System.out.println("2. Non");
+
+        System.out.print("Votre choix : ");
+        int choix = 0;
+        try {
+            choix = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Erreur de saisie, application lancée sans les données de test");
+        }
+
+        if (choix == 1) {
+            gestionnaireFraudes = JeuDonneesTest.creerDonneesTest();
+            System.out.println("Données de test chargées.");
+        } else {
+            gestionnaireFraudes = new GestionnaireFraudes();
+            System.out.println("Application lancée sans données de test.");
+        }
+
+        MenuConsole menuConsole = new MenuConsole(gestionnaireFraudes);
+        menuConsole.lancer();
     }
 }
